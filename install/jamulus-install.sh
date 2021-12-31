7e7e8e0f2d8f139b1b9c9847ae652516f3a59a16
ExtelligenceIT <34581471+ExtelligenceIT@users.noreply.github.com>
2021-12-31 14:08:36 +0000

    Updated to include environment

--- install/jamulus-install.sh
+++ install/jamulus-install.sh
@@ -40,8 +40,67 @@ else
     echo "**** Remove Build Packages ****"
     apk del .build-dependencies
 
+    echo "**** Add additional Packages ****"
     apk add --update --no-cache \
-        qt5-qtbase-x11 icu-libs tzdata
+        qt5-qtbase-x11 icu-libs tzdata busybox-extras
+
+    rc-update add httpd
 
     echo "Jamulus Version $JAMULUS_VERSION Installed at: /usr/local/bin/Jamulus"
+
+    echo "**** Creating Jamulus Environment ****"
+
+    echo " - Creating web and recording directories"
+    mkdir -p /jamulus/www
+    mkdir -p /jamulus/rec
+
+    if [ -r /jamulus/directoryserver ] && [ -s /jamulus/directoryserver ]
+    then
+        echo " - /jamulus/directoryserver already exists - keeping"
+    else
+        echo " - Creating default directoryserver file"
+        echo "anygenre2.jamulus.io:22224" > /jamulus/directoryserver
+    fi
+
+    if [ -r /jamulus/serverinfo ] && [ -s /jamulus/serverinfo ]
+    then
+        echo " - /jamulus/serverinfo already exists - keeping"
+    else
+        echo " - Creating default serverinfo file"
+        echo "[name];[city];[locale value]" > /jamulus/serverinfo
+    fi
+
+    if [ -r /jamulus/welcome.html ]
+    then
+        echo " - /jamulus/welcome.html already exists - keeping"
+    else
+        echo " - creating default welcome file"
+        echo "<html>\n<body>\n</body>\n</html>" > /jamulus/welcome.html
+    fi
+
+    if [ -r /etc/init.d/jamulus-headless]
+    then
+        echo " - jamulus service already exists - keeping"
+    else
+        echo " - Setting up jamulus service"
+        wget -P /etc/init.d https://raw.githubusercontent.com/ExtelligenceIT/jamulus-headless-alpine/main/etc/init.d/jamulus-headless
+        chmod 755 /etc/init.d/jamulus-headless
+        echo " - enabling service jamulus-headless to run on startup"
+        rc-update add jamulus-headless
+    fi
+
+    if [ -r /etc/httpd.conf]
+    then
+        echo " - httpd.conf already exists - confirm it contains /jamulus/www"
+    else
+        echo " - creating /etc/httpd.conf"
+        echo "H:/jamulus/www" > /etc/httpd.conf
+        echo " - enabling service httpd to run on startup"
+        rc-update add httpd
+    fi
+
+
+
+    
+    
 fi
